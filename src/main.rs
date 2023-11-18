b05765b14997c9369da67a9a611eedff4affb6a4
Matt Palmer <palmer.s.matt@gmail.com>
2023-11-18 11:56:45 -0500

    Handle form POST variant of /authorize

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rocket::form::Form;
+
 #[macro_use]
 extern crate rocket;
 
@@ -13,21 +15,38 @@ struct AuthorizePayload {
     redirect_uri: String,
     scope: String,
     state: String,
+    nonce: Option<String>,
+}
+
+impl ToString for AuthorizePayload {
+    fn to_string(&self) -> String {
+        format!(
+            "response_type={:?}, client_id={}, redirect_uri={}, scope={}, state={}, nonce={:?}",
+            self.response_type,
+            self.client_id,
+            self.redirect_uri,
+            self.scope,
+            self.state,
+            self.nonce
+        )
+    }
+}
+
+fn handle_authorize(payload: AuthorizePayload) -> String {
+    format!("Hello, world! {}", payload.to_string())
 }
 
 #[get("/authorize?<payload..>")]
-fn authorize(payload: AuthorizePayload) -> String {
-    format!(
-        "Hello, {}! response_type={:?}, redirect_uri={}, scope={}, state={}",
-        payload.client_id,
-        payload.response_type,
-        payload.redirect_uri,
-        payload.scope,
-        payload.state
-    )
+fn authorize_get(payload: AuthorizePayload) -> String {
+    handle_authorize(payload)
+}
+
+#[post("/authorize", data = "<payload>")]
+fn authorize_post(payload: Form<AuthorizePayload>) -> String {
+    handle_authorize(payload.into_inner())
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![authorize])
+    rocket::build().mount("/", routes![authorize_get, authorize_post])
 }
