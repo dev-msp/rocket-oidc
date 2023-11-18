521e4ffa97bbcd050a8fe1d2de020701a7c7f7fa
Matt Palmer <palmer.s.matt@gmail.com>
2023-11-18 11:52:49 -0500

    Initialize authorize endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn root() -> &'static str {
-    "Hello, world!"
+#[derive(Debug, FromFormField)]
+enum ResponseType {
+    Code,
+}
+
+#[derive(FromForm)]
+struct AuthorizePayload {
+    response_type: ResponseType,
+    client_id: String,
+    redirect_uri: String,
+    scope: String,
+    state: String,
+}
+
+#[get("/authorize?<payload..>")]
+fn authorize(payload: AuthorizePayload) -> String {
+    format!(
+        "Hello, {}! response_type={:?}, redirect_uri={}, scope={}, state={}",
+        payload.client_id,
+        payload.response_type,
+        payload.redirect_uri,
+        payload.scope,
+        payload.state
+    )
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![root])
+    rocket::build().mount("/", routes![authorize])
 }
